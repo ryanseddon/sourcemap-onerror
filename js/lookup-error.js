d573a3d9988d76a663a26f8a35873f8e2fcd4fbb
Ryan Seddon <seddon.ryan@gmail.com>
2013-09-07 16:39:34 +1000

    Added trycatch, and stack trace parsing
    * Trycatch example doing source map lookup
    * Added stack trace parsing to get line/column no.

--- js/lookup-error.js
+++ js/lookup-error.js
@@ -1,17 +1,58 @@
 var fizz_source_map = new sourceMap.SourceMapConsumer(fizzbuzz_map);
 var test_source_map = new sourceMap.SourceMapConsumer(test_map);
-var html = document.documentElement;
+var catch_source_map = new sourceMap.SourceMapConsumer(catch_map);
+var body = document.body;
+
+function parseStackTrace(err) {
+    var info;
+    var source_map;
+
+    if(err.mode === 'trycatch') {
+        info = parseTryCatchStackTrace(err);
+        source_map = catch_source_map;
+    } else {
+        info = err.stack;
+
+        source_map = test_source_map;
+        if(/fizzbuzz/.test(info.filename)) {
+            source_map = fizz_source_map;
+        }
+    }
+    
+    
+    var orig = source_map.originalPositionFor({
+            line: info.line,
+            column: info.column
+    });
+
+    orig.mode = err.mode;
+    
+    body.innerHTML += '<div class="error"><pre>' + JSON.stringify(orig, null, 4);
+}
+
+function parseTryCatchStackTrace(err) {
+    var lines = err.stack.split('\n'),
+        stack;
+
+    stack = lines[1].split(':').reverse().splice(0, 2);
+
+    return {
+        column: 0,
+        line: stack[1]
+    };
+}
 
 window.onerror = function(msg, filename, lineno, colno, err) {
-    var source_map = test_source_map;
-    if(/fizzbuzz/.test(filename)) {
-        source_map = fizz_source_map;
-    }
-    var orig = source_map.originalPositionFor({
+    var error = {
+        err: err,
+        stack: {
+            message: msg,
             line: lineno,
-            column: colno
-    });
-    
-    html.appendChild(document.createTextNode(JSON.stringify(orig)));
-    //alert(JSON.stringify(orig));
+            column: colno,
+            filename: filename
+        },
+        mode: 'onerror'
+    };
+
+    parseStackTrace(error);
 };
